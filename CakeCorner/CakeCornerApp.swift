f85243ec461b9672c3336280071fb145c12006ac
abdiwali96 <abdiwali96@gmail.com>
2020-09-29 15:23:10 +0100

    Initial Commit

---
+++ CakeCorner/CakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CakeCornerApp.swift
+//  CakeCorner
+//
+//  Created by Abdiwali Abdi on 29/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct CakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
